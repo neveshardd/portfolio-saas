2d113eeeaa344b2bc229f11d5ea66bd300a08782
Sqlitt <118855875+sqlitt@users.noreply.github.com>
2025-03-04 16:22:50 -0300

    create frontend layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { CirclePlus, Download, Trash2 } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
     return (
         <div className="text-white px-4 py-3 space-y-12">
+            <button className="bg-white text-black p-3 rounded-full cursor-pointer bottom-0 right-10 fixed"><Download /></button>
             <div className="flex justify-between items-center">
                 <div className="font-bold">Cliente</div>
                 <Image src="/logo.png" width={50} height={50} alt="logo" />
@@ -13,24 +15,24 @@ export default function Home() {
                     <div>Mobile</div>
                     <button className="bg-white text-black py-2 px-6 rounded-full">Adicionar +</button>
                 </div>
-                <div className="grid grid-cols-2 gap-4">
-                    <div className="border border-dashed flex justify-center items-center w-42 h-64">+</div>
-                    <div className="border border-dashed flex justify-center items-center w-42 h-64">+</div>
-                    <div className="border border-dashed flex justify-center items-center w-42 h-64">+</div>
-                    <div className="border border-dashed flex justify-center items-center w-42 h-64">+</div>
-                    <div className="border border-dashed flex justify-center items-center w-42 h-64">+</div>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-2 md:gap-4">
+                    <div className="border border-dashed flex justify-center items-center w-full aspect-[9/16] min-h-32 relative">
+                        <button className="absolute right-5 top-5 cursor-pointer bg-red-500 text-white p-3 rounded-full"><Trash2 /></button>
+                        <div><CirclePlus /></div>
+                    </div>
                 </div>
             </div>
 
             <div className="space-y-6">
                 <div className="flex justify-between items-center">
                     <div>Desktop</div>
-                    <button className="bg-white text-black py-2 px-6 rounded-full">Adicionar +</button>
+                    <button className="bg-white text-black py-2 px-4 md:px-6 rounded-full">Adicionar +</button>
                 </div>
-                <div className="gap-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-                    <div className="border flex justify-center items-center w-full h-48">+</div>
-                    <div className="border flex justify-center items-center w-full h-48">+</div>
-                    <div className="border flex justify-center items-center w-full h-48">+</div>
+                <div className="gap-3 flex flex-col">
+                    <div className="border border-dashed flex justify-center items-center w-full aspect-video relative">
+                    <button className="absolute right-5 top-5 cursor-pointer bg-red-500 text-white p-3 rounded-full"><Trash2 /></button>
+                    <div><CirclePlus /></div>
+                    </div>
                 </div>
             </div>
 
